5a752b93781ade4106d6cff58779e056882b2326
Okwuzu Chiagozie <okwuzuchiagozie@gmail.com>
2020-05-23 22:16:55 +0100

    add preloader

--- home/static/home/js/app.js
+++ home/static/home/js/app.js
@@ -1,8 +1,13 @@
 $(function(){
+    /* remove preloader*/
+    $('#preloader').fadeOut();
     /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
     const particles_url = $('#particles-json-url').val();
     particlesJS.load('particles-js', particles_url, function() {
         console.info('callback - particles.js config loaded');
     });
-
+    /* Initialize tooltips */
+    $('[data-toggle="tooltip"]').tooltip({
+        container: 'body',
+    })
 });
