b91f66df31fe88f28d3bc45b7d0bd388d96a8960
Okwuzu Chiagozie <okwuzuchiagozie@gmail.com>
2020-06-23 21:54:45 +0100

    setup dashboard lock screen

--- dashboard/static/dashboard/js/app.js
+++ dashboard/static/dashboard/js/app.js
@@ -5,14 +5,81 @@ $(function(){
         router,
         data(){
             return {
-
+                screen_lock_settings: {
+                    pressed: false,
+                    last_position: {},
+                    default_height: 0,
+                    new_height: 0,
+                    screen_height: screen.height
+                },
+            }
+        },
+        watch: {
+            'screen_lock_settings.pressed': function(newVal){
+                if(!newVal){
+                    let half = (this.screen_lock_settings.screen_height / 2);
+                    if(this.screen_lock_settings.new_height > half){
+                        this.screen_lock_settings.new_height = this.screen_lock_settings.screen_height
+                    }else{
+                        this.screen_lock_settings.new_height = this.screen_lock_settings.default_height
+                    }
+                }
             }
         },
         methods: {
+            mouse_move_on_locker_screen(event){
+                if(this.screen_lock_settings.pressed){
+                    if(this.screen_lock_settings.last_position.x !== undefined){
+                        let deltaX = (event.offsetX - this.screen_lock_settings.last_position.x);
+                        let deltaY = (event.offsetY - this.screen_lock_settings.last_position.y);
+                        let absDeltaX = Math.abs(deltaX);
+                        let absDeltaY = Math.abs(deltaY);
+                        let new_height = this.screen_lock_settings.new_height;
+                        let default_height = this.screen_lock_settings.default_height;
+                        let screen_height = this.screen_lock_settings.screen_height;
 
+                        // mouse movement is up
+                        if((absDeltaY > absDeltaX) && deltaY < 0){
+                            if(new_height > default_height){
+                                new_height -= 10;
+                            }else{
+                                new_height = default_height
+                            }
+                        }
+                        // mouse movement is down
+                        else if((absDeltaY > absDeltaX) && deltaY > 0){
+                            if(new_height <= screen_height){
+                                new_height += 10;
+                            }else{
+                                new_height = screen_height
+                            }
+                        }
+                        this.screen_lock_settings.new_height = new_height
+                    }
+                    this.screen_lock_settings.last_position = {
+                        x: event.offsetX,
+                        y: event.offsetY
+                    }
+                }else{
+
+                }
+            },
+            toggleScreenHeight(){
+                let new_height = this.screen_lock_settings.new_height;
+                let screen_height = this.screen_lock_settings.screen_height;
+                let default_height = this.screen_lock_settings.default_height;
+                if(new_height === screen_height){
+                    new_height = default_height
+                }else{
+                     new_height = screen_height
+                }
+                this.screen_lock_settings.new_height = new_height
+            },
         },
         created(){
-
+            // get default height of lock screen
+            this.screen_lock_settings.default_height = this.screen_lock_settings.new_height =
+                Number.parseFloat($('#app-locker-screen').css('height').replace('px', ''));
         },
         mounted(){
             /* remove preloader*/
--- dashboard/static/dashboard/js/router.js
+++ dashboard/static/dashboard/js/router.js
@@ -9,3 +9,32 @@ const router = new VueRouter({
     mode: 'history',
     routes
 });
+
+// var last_position = {},
+//     $output       = $('#output');
+// $(document).on('mousemove', function (event) {
+//     if (typeof(last_position.x) != 'undefined') {
+//         var deltaX = last_position.x - event.offsetX,
+//             deltaY = last_position.y - event.offsetY;
+//         if (Math.abs(deltaX) > Math.abs(deltaY) && deltaX > 0) {
+//             //left
+//             $output.append('<li>Left</li>');
+//         } else if (Math.abs(deltaX) > Math.abs(deltaY) && deltaX < 0) {
+//             //right
+//             $output.append('<li>Right</li>');
+//         } else if (Math.abs(deltaY) > Math.abs(deltaX) && deltaY > 0) {
+//             //up
+//             $output.append('<li>Up</li>');
+//         } else if (Math.abs(deltaY) > Math.abs(deltaX) && deltaY < 0) {
+//             //down
+//             $output.append('<li>Down</li>');
+//         }
+//         if ($output.children().length > 4) {
+//             $output.children().eq(0).remove();
+//         }
+//     }
+//     last_position = {
+//         x : event.offsetX,
+//         y : event.offsetY
+//     };
+// });
